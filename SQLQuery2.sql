bb103c0246f76c5bb1b2a478c27d123087f67187
jansenv <jansenv44@gmail.com>
2020-04-07 12:51:09 -0500

    finished num 1

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿--1
+
+SELECT d.[Name] AS 'Department Name', emp.LastName, emp.FirstName,
+CASE WHEN emp.IsSupervisor = 1 THEN 'True' else 'False' END AS Supervisor
+FROM Employee emp
+LEFT JOIN Department d 
+ON emp.DepartmentId = d.Id
+ORDER BY [Department Name], LastName, FirstName
+
+--2
+
