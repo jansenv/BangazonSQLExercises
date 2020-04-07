3a64eb88fa7ca313f809640a642fe26c92cb9b74
jansenv <jansenv44@gmail.com>
2020-04-07 13:17:35 -0500

    finished 5

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,31 +1,36 @@
 ﻿--1
 
-SELECT d.[Name] AS 'Department Name', emp.LastName, emp.FirstName,
-CASE WHEN emp.IsSupervisor = 1 THEN 'True' ELSE 'False' END AS Supervisor
-FROM Employee emp
-LEFT JOIN Department d 
-ON emp.DepartmentId = d.Id
-ORDER BY [Department Name], LastName, FirstName
+--SELECT d.[Name] AS 'Department Name', emp.LastName, emp.FirstName,
+--CASE WHEN emp.IsSupervisor = 1 THEN 'True' ELSE 'False' END AS Supervisor
+--FROM Employee emp
+--LEFT JOIN Department d 
+--ON emp.DepartmentId = d.Id
+--ORDER BY [Department Name], LastName, FirstName
 
 --2
 
-SELECT [Name], Budget
-FROM Department
-ORDER BY Budget DESC
+--SELECT [Name], Budget
+--FROM Department
+--ORDER BY Budget DESC
 
 --3
 
-SELECT d.[Name], emp.FirstName, emp.LastName,
-CASE WHEN emp.IsSupervisor = 1 THEN 'True' ELSE 'False' END AS Supervisor
-FROM Department d
-LEFT JOIN Employee emp
-ON d.Id = emp.DepartmentId
-WHERE emp.IsSupervisor = 1;
+--SELECT d.[Name], emp.FirstName, emp.LastName,
+--CASE WHEN emp.IsSupervisor = 1 THEN 'True' ELSE 'False' END AS Supervisor
+--FROM Department d
+--LEFT JOIN Employee emp
+--ON d.Id = emp.DepartmentId
+--WHERE emp.IsSupervisor = 1;
 
 --4
 
-SELECT COUNT(e.Id) AS 'Count of Employees in Department'
-FROM Employee e
-LEFT JOIN Department d
-ON d.Id = e.DepartmentId
-GROUP BY d.[Name]
+--SELECT d.[Name], COUNT(e.Id) AS 'Count of Employees in Department'
+--FROM Employee e
+--LEFT JOIN Department d
+--ON d.Id = e.DepartmentId
+--GROUP BY d.[Name]
+
+--5
+
+--UPDATE Department
+--SET Budget = Budget * 1.2
